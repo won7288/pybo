452ebed7bcc0ee8015cbe33ad09f244c5bbd8d9e
won7288 <won72881@naver.com>
2023-04-11 20:52:59 +0900

    파이보 로그 적용

--- config/settings/base.py
+++ config/settings/base.py
@@ -185,15 +185,15 @@ LOGGING = {
         'django': {
             'handlers': ['console', 'mail_admins', 'file'],
             'level': 'INFO',
+            'pybo': {
+                'handlers': ['console', 'file'],
+                'level': 'INFO',
+            }
         },
         'django.server': {
             'handlers': ['django.server'],
             'level': 'INFO',
             'propagate': False,
-            'pybo': {
-                'handlers': ['console', 'file'],
-                'level': 'INFO',
-            }
         },
     }
 }
